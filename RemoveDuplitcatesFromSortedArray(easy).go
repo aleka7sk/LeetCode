43f6050cd132952a22d4afb17a64402f4922bbed
aleka7sk <aleka425131@gmail.com>
2022-04-01 13:55:52 +0600

    removeDuplicates better version

--- RemoveDuplitcatesFromSortedArray(easy).go
+++ RemoveDuplitcatesFromSortedArray(easy).go
@@ -1,16 +1,29 @@
 package leetcode
 
+// func removeDuplicates(nums []int) int {
+// 	index := 0
+// 	for i := 0; i < len(nums); i++ {
+// 		if nums[i] != -101 {
+// 			nums[index] = nums[i]
+// 			index++
+// 		}
+// 		for j := i + 1; j < len(nums); j++ {
+// 			if nums[i] == nums[j] {
+// 				nums[j] = -101
+// 			}
+// 		}
+// 	}
+// 	return index
+// }
+
 func removeDuplicates(nums []int) int {
-	index := 0
+	index := 1
+	var temp = nums[0]
 	for i := 0; i < len(nums); i++ {
-		if nums[i] != -101 {
+		if nums[i] > temp {
+			temp = nums[i]
 			nums[index] = nums[i]
 			index++
-		}
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i] == nums[j] {
-				nums[j] = -101
-			}
 		}
 	}
 	return index
