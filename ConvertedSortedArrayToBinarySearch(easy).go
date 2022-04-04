c3d86054776023d435a40de0150834df840ae727
aleka7sk <aleka425131@gmail.com>
2022-04-05 03:46:27 +0600

    ConvertedSortedArrayToBInarySearch

---
+++ ConvertedSortedArrayToBinarySearch(easy).go
@@ -0,0 +1,27 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	mid := len(nums) / 2
+	root := &TreeNode{Val: nums[mid]}
+	root.Left = sortedArrayToBST(nums[0:mid])
+	root.Right = sortedArrayToBST(nums[mid+1 : len(nums)])
+	return root
+}
