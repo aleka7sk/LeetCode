5eb44624da59d23d24b2cca4f845aadc93104327
aleka7sk <aleka425131@gmail.com>
2022-06-13 01:56:28 +0600

    PascalTriangle updated with recursion solution

--- Pascal'sTriangle(easy).go
+++ Pascal'sTriangle(easy).go
@@ -1,19 +1,22 @@
 package leetcode
 
 func generate(numRows int) [][]int {
-	array := [][]int{}
-	for i := 0; i < numRows; i++ {
-		arr := []int{}
-		for j := 0; j <= i; j++ {
-			if j == 0 {
-				arr = append(arr, 1)
-			} else if j == i {
-				arr = append(arr, 1)
-			} else {
-				arr = append(arr, array[i-1][j-1]+array[i-1][j])
-			}
-		}
-		array = append(array, arr)
-	}
-	return array
+    grid := [][]int{[]int{1}}
+    Recursion(numRows, 2, []int{1}, &grid)
+    return grid
 }
+
+func Recursion(size int, col int, arr []int, grid *[][]int){
+    if col > size {
+        return 
+    }
+    array := []int{}
+    array = append(array, 1)
+    for i := 0; i < len(arr) - 1; i++ {
+        array = append(array, arr[i] + arr[i+1])
+    }
+    array = append(array, 1)
+    *grid = append(*grid, array)
+    Recursion(size, col + 1, array, grid)
+}
+
